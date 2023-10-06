c0b77146559dea24c4688c30f949d821a88c4c4f
Sulthon-Baiquni <sulthonbaiquni05@gmail.com>
2023-10-06 14:32:41 +0700

    rename

--- lib/nomor2.dart
+++
@@ -1,9 +0,0 @@
-void main(){
-  String text = 'Rumahku di JAkarta';
-
-  String text1 = text.toUpperCase();
-  // String text2 = text.();
-
-  print('$text1');
-}
-
--- lib/nomor3.dart
+++
@@ -1,6 +0,0 @@
-void main(){
-  String teks = 'Ini adalah Rosid';
-  String kata = teks.substring(11,16);
-
-  print('$kata');
-}
